006ff4b5ead41bac4388e05bd288c4749890c650
sakana89 <nakarashi816@gmail.com>
2024-10-17 12:59:26 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,17 @@
+public adstract class TagibleAsset{
+private String name;
+private int price;
+private String color;
+
+public TangibleAsset(String name,int price,String color){
+this.name = name;
+this.price = price;
+this.color = color;
+
+}
+
+public String getName(){retun this.name;}
+public String getPrice(){retun this.price;}
+public String getColor(){retun this.color;}
+
+}
